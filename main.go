ca74ca70abd69a7f62b03edde3a8223dd5c4f2b9
Yash Gaitonde <gaitoyas20@gmail.com>
2020-08-07 19:36:42 -0400

    parsing lines

--- main.go
+++ main.go
@@ -19,9 +19,25 @@ func main() {
 	if err != nil {
 		exit("Failed to parse the provided CSV file")
 	}
-	fmt.Println(lines)
+	problems := parseLines(lines)
+	fmt.Println(problems)
 }
 
+type problem struct {
+	q string
+	a string
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: line[1],
+		}
+	}
+	return ret
+}
 
 func exit(msg string){
 	fmt.Println(msg)
