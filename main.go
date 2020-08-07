72b569ffe6f443ca1225e968aaf0aa10a2bc73b3
Yash Gaitonde <gaitoyas20@gmail.com>
2020-08-07 19:30:37 -0400

    print csv

--- main.go
+++ main.go
@@ -1,8 +1,29 @@
 package main
-import "flag"
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
 	filename := flag.String("csv", "problems.csv", "a csv file in the format: 'question,answer")
 	flag.Parse()
-	_ = filename
+	
+	file, err := os.Open(*filename)
+	if err!=nil {
+		exit(fmt.Sprintf("Failed to open %s", *filename))
+	}
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse the provided CSV file")
+	}
+	fmt.Println(lines)
+}
+
+
+func exit(msg string){
+	fmt.Println(msg)
+	os.Exit(1)
 }
